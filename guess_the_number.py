962fc30b8f9cba4b0a634314156fc52316fcc252
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-20 11:38:30 -0700

    added more to the for loop

--- guess_the_number.py
+++ guess_the_number.py
@@ -30,7 +30,6 @@
 
 import random 
 # how does this import work? importing from where?
-guesses_taken = 0
 user_name = raw_input("Hello there! Whats your name? ")
 print "Hi" + user_name "We're gonna play a number guessing game!"
 print "lets play!" # whats the syntax for  a string to break into a new line without creating a new print
@@ -38,7 +37,26 @@ print "lets play!" # whats the syntax for  a string to break into a new line wit
 number = random.randint(3,45)
 
 
-for n in range(8):
+for n in range(8): # why range 8?
 	print "Okay" + user_name " take a guess pick a number between 4 and 44"
 	user_guess = int(raw_input("> "))
-	
+
+	if user_guess > number:
+		print user_name + " your number is to big! Try again!"
+	elif user_guess < number:
+		print user_name + " your number is too small! Try again!"
+	elif user_guess == number:
+		print "You did it " + user_name + "! Good Job!"
+		break
+	else:
+		print "Nope."
+		print "Just nope. its fucked"
+
+
+
+
+
+
+
+
+
