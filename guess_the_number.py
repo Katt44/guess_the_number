782d78230fc4dbb2f584de506133c54a571e4a1f
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-20 11:16:57 -0700

    Reworded pseudo code and commented out first part of code

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,22 +1,26 @@
 #Pseudo code:
 #part 1
-# to  gain user input guessing that number.
+# Set up conditonals to have the user guess a number
 # I want to let the user when their number is to big or too small
 #Part 2
-# randomly generated numbers
+# Set up  conditionals that the user guess a number that is randomly generated.
 # part 3
 # I want it to do part 1 but be able to take a range of numbers from user.
 
-secret_num = 23
 
-user_guess = int(raw_input("Guess a number between 1 and 25: "))
+# Part 1
 
-if user_guess > secret_num:
-	print "Your number is too big!"
-elif user_guess < secret_num:
-	print "your number is too small!"
-elif user_guess == secret_num:
-	print "Yay thats the number!"
-else: 
-	print "Something went wrong!"
+
+#secret_num = 23
+
+#user_guess = int(raw_input("Guess a number between 1 and 25: "))
+
+#if user_guess > secret_num:
+#	print "Your number is too big!"
+#elif user_guess < secret_num:
+#	print "your number is too small!"
+#elif user_guess == secret_num:
+#	print "Yay thats the number!"
+#else: 
+#	print "Something went wrong!"
 
