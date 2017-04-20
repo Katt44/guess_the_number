456a15ed3edce4bd3dc8077e5edb67851c2bbbd6
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-19 23:35:34 -0700

    added to pseudo code

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,7 +1,10 @@
 #Pseudo code:
 #part 1
-# I want to randomly generate a number and  gain user input guessing that number.
+# to  gain user input guessing that number.
 # I want to let the user when their number is to big or too small
-# part 2
+#Part 2
+# randomly generated numbers
+# part 3
 # I want it to do part 1 but be able to take a range of numbers from user.
+
 
